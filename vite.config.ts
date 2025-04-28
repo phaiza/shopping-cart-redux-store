eeeca8c985088b14772d5fd07d54f3cd73518705
fahmed1 <fahmed1@woolworths.com.au>
2025-04-29 09:56:11 +1000

    Fix vite base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shopping-cart-redux-store', // <-- your repo name here!
+  base: '/shopping-cart-redux-store/',
   plugins: [react()],
 });
